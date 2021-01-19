1c8acf8728e991ed4b2686932dcd448fc1c5b3a1
Almir Muminovic <projectalmir@gmail.com>
2021-01-19 22:11:51 +0100

    Adding more Routes to the API

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
-    });
-})
+const orderRoutes = require('./api/routes/orders')
+const productRoutes = require('./api/routes/products')
+
+app.use('/products', productRoutes)
+app.use('/orders', orderRoutes)
 
 module.exports = app;
